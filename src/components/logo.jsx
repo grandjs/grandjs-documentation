ad0b609df96d5cc3ebb5ceade1aac2f2ff06a238
tareksalem <tarekpopp@gmail.com>
2020-06-19 18:57:23 +0200

    updating the documentation logo

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const LogoComponent = () => {
     return (
+        <div style={{width: "100%", height: 300}}>
         <div style={{width: "300px", margin: "auto", height: 300}}>
         <img src="https://i.ibb.co/t4MDdkq/grandjs.png" style={{width: "100%", height: "100%"}}/>
+        </div>
         </div>
     )
 }
