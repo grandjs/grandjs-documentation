a7b702749fc580db9393f9f8f1c5acac3253ba44
tareksalem <tarekpopp@gmail.com>
2020-06-19 18:01:24 +0200

    grandjs framework documentation

---
+++ doczrc.js
@@ -0,0 +1,7 @@
+export default {
+    title: "Grandjs Framework",
+    description: "Grandjs is an extendable nodejs framework based on typescript for building extensible and solid applications",
+    ignore: [
+        "README.md"
+    ]
+}
---
+++ src/components/logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const LogoComponent = () => {
+    return (
+        <div style={{width: "300px", margin: "auto", height: 300}}>
+        <img src="https://i.ibb.co/t4MDdkq/grandjs.png" style={{width: "100%", height: "100%"}}/>
+        </div>
+    )
+}
+
+
+
+export {LogoComponent};
