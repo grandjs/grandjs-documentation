79abf1fef66d419842a5ee9bf31d820aa6263129
tareksalem <tarekpopp@gmail.com>
2020-12-11 20:46:36 +0200

    implement middleWares implementation definission

---
+++ setAliases.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = (config) => {
+    const newalias = {
+        '@public': path.join(__dirname, '/public'),
+    };
+
+    config.resolve.alias = { ...config.resolve.alias, ...newalias };
+    return config;
+};
